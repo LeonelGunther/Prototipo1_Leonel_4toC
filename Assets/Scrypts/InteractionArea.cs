d63a55c233dd509dc1ecb205032e108a4631c390
Papaleo (homo) <49980496@est.ort.edu.ar>
2026-05-21 11:56:25 -0300

    Clase 3: Implementación de GameManager y UIManager con TextMeshPro"

--- Assets/Scrypts/InteractionArea.cs
+++ Assets/Scrypts/InteractionArea.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class InteractionArea : MonoBehaviour
 {
+    public int score = 0;
+    public UIManager uiMaragerScript;
+
+
+void Awake(){
+    uiMaragerScript = GameObject.FindObjectOfType<UIManager>() ;
+}
+
     void OnTriggerEnter(Collider other){
         Debug.Log(other.gameObject.name);
             if (other.gameObject.CompareTag("Collectible")){
                 Destroy(other.gameObject);
+                score ++;
+                uiMaragerScript.UpdateScore(score);
             }
     }
 }
